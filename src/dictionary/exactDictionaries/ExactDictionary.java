001296f6aa5d285a41c7e6ab37d62024be6f710a
jaymdq <bcaimmi@gmail.com>
2015-06-01 01:22:14 -0300

    Pushed

--- src/dictionary/exactDictionaries/ExactDictionary.java
+++ src/dictionary/exactDictionaries/ExactDictionary.java
@@ -29,8 +29,6 @@ public class ExactDictionary implements Dictionary {
 	public ExactDictionary (Vector<DictionaryEntry> entries,boolean caseSensitive, boolean allMatches){
 		DictionaryNode root = new DictionaryNode(0);
 		for (DictionaryEntry entry : entries){
-			//if (!caseSensitive)
-			//	entry.setText(entry.getText().toLowerCase());
 			Integer length = root.addEntry(Segmenter.getSegmentation(entry.getText(),caseSensitive,true),entry);
 			if (length > maxLength)
 				maxLength = length;			
